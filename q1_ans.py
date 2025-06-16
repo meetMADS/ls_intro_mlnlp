53e39a35ce27cbc510b20c231de6edfb2eeadcdf
meetMADS <162668259+meetMADS@users.noreply.github.com>
2025-06-16 22:46:57 +0530

    Update q2.py

--- q1_ans.py
+++ q1_ans.py
@@ -1,27 +1,28 @@
 import numpy as np
 
-np.random.seed(0)  
-arr = np.random.randint(1, 51, size=(5, 4))
-print("Array:\n", arr)
+arr = np.random.uniform(0, 10, 20)
+rounded_arr = np.round(arr, 2)
+print("Original Array : \n", rounded_arr)
 
-element_cross = [arr[i, -i-1] for i in range(min(arr.shape))]
+mini = np.min(rounded_arr)
+maxi = np.max(rounded_arr)
+median = np.median(rounded_arr)
+print(f"Minimum: {mini}, Maximum: {maxi}, Median: {median}")
 
-row_max = np.max(arr, axis=1)
-print("Max value in each row:", row_max)
+modified_arr = np.where(rounded_arr < 5, np.round(rounded_arr**2, 2), rounded_arr)
+print("Modified Array :\n", modified_arr)
 
-mean_value = np.mean(arr)
-sorted = arr[arr <= mean_value]
-
-def numpy_boundary_traversal(matrix):
-    if matrix.size == 0:
-        return []
-
-    top = matrix[0, :]
-    right = matrix[1:-1, -1]
-    bottom = matrix[-1, ::-1]
-    left = matrix[-2:0:-1, 0]
-
-    boundary = np.concatenate((top, right, bottom, left)).tolist()
-    return boundary
-
-peripheral_elements = numpy_boundary_traversal(arr)
+def numpy_alternate_sort(array):
+    sorted_arr = np.sort(array)
+    result = []
+    left = 0
+    right = len(sorted_arr) - 1
+    while left <= right:
+        if left == right:
+            result.append(sorted_arr[left])
+        else:
+            result.extend([sorted_arr[left], sorted_arr[right]])
+        left += 1
+        right -= 1
+    return np.array(result)
+new_array = numpy_alternate_sort(rounded_arr)
